ba2a334f5745883361afb9c45c01a8a7bfb99a22
someguy235 <someguy235@gmail.com>
2013-11-22 12:05:47 -0500

    layer-based markers working, basic feature-complete

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
 
 app.engine('.jade', require('jade').__express);
-//app.engine('.html', require('jade').__express);
 
 app.get('/', function(req, res){
   res.send("that's not a page", 404);
@@ -27,67 +26,36 @@ app.get('/avlcrime', function(req, res){
 });
 
 app.post('/avlcrime/crimes', function(req, response){
-  //console.log("crimes");
-  var params = req.body, maxYear = [], minYear = [];
-  //console.log("params: "+ JSON.stringify(params));
-  /*
-  if(params.years.length){
-    var maxYear = (parseInt(Math.max.apply(null, params.years)) + 1) + "-01-01 00:00:00.0";
-    var minYear = Math.min.apply(null, params.years) + "-01-01 00:00:00.0";
-  }
-  */
-  db.crimes.find({ 
-    //$and:[
-    //  {"properties.thedate": {$gte: minYear}}, 
-    //  {"properties.thedate": {$lt: maxYear}}
-    //],
-    //"properties.severity": { $in: params.severities }, 
-    "properties.offense": { $in: params.offenses } }, 
+  db.crimes.find({}, 
     function(err, crimes){
     if(err){
       console.log("err: "+ err);
     }else{
-      //console.log("crimes: "+ crimes.length);
-      var fc = {type: "FeatureCollection", features: crimes}
+      var fc = {}
+      _.each(crimes, function(val, key){
+        if(typeof fc[val.properties.offense] == 'undefined'){
+          fc[val.properties.offense] = [];
+        }
+        fc[val.properties.offense].push({
+          lat: val.geometry.coordinates[1],
+          lon: val.geometry.coordinates[0],
+          off: val.properties.offense,
+          add: val.properties.address,
+          date: val.properties.thedate
+        });
+      })
       response.send(fc, 200);
     }
   })
 });
 
 app.get('/avlcrime/params', function(req, response){
-  var params = {
-    offenses: [],
-    severities: [],
-    years: []
-  }
-
-  async.parallel({
-    severities: function(callback){
-      db.crimes.distinct('properties.severity', function(err, sev){
-        if(err){
-          console.log(err);
-        }else{
-          params.severities = sev;
-        }
-        callback(null, sev);
-      });
-    },
-    offenses: function(callback){
-      db.crimes.distinct('properties.offense', function(err, off){
-        if(err){
-          console.log(err);
-        }else{
-          params.offenses = off;
-        }
-        callback(null, off);
-      });
-    },
-    years: function(callback){
-      callback(null, ['2012', '2013']);
-    },
-  },
-  function(err, result){
-    response.send(result, 200);
+  db.crimes.distinct('properties.offense', function(err, off){
+    if(err){
+      console.log(err);
+    }else{
+      response.send(off, 200);
+    }
   });
 });
 
--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -4,7 +4,7 @@
  */
 
 var events, showSpinner = false, showError = false, showChart = false;
-var offenses, severities, years;
+var offenses, layers = {};
 var FILL_COLORS = {
   'Drug Arrest': '#FFC73F',
   'Vandalism': '#E8903A',
@@ -16,15 +16,13 @@ var FILL_COLORS = {
   'Rape': '#47FFAD',
   'Homicide': '#4CE835'
 }
-//var offenses = null, severities = null, years = null;
 
 var CrimeModule = angular.module('CrimeApp', []);
 
 CrimeModule.factory('CrimeService', function(){
   var service = {
     offenses: offenses,
-    severities: severities,
-    years: years
+    layers: layers
   }
   return service
 })
@@ -68,14 +66,14 @@ CrimeModule.controller("FormController", function($scope, $http, CrimeService){
     url: '/avlcrime/params',
     method: "GET",
     headers: {'Content-Type': 'application/json'}
-  }).success(function (data, status, headers, config) {
+  }).success(function (offenses, status, headers, config) {
     var tmpOff = []
-    for(var i=0; i<data.offenses.length; i++){
-      tmpOff.push({name: data.offenses[i], selected: false, color: FILL_COLORS[data.offenses[i]]})
+    for(var i=0; i<offenses.length; i++){
+      tmpOff.push({name: offenses[i], selected: false, color: FILL_COLORS[offenses[i]]})
     }
     $scope.offenses = tmpOff;
   }).error(function (data, status, headers, config) {
-    console.log("error")
+    //console.log("error")
   });
   $scope.$watch('offenses', function(newVal, oldVal, scope){
     if(newVal !== oldVal){
@@ -87,8 +85,8 @@ CrimeModule.controller("FormController", function($scope, $http, CrimeService){
 CrimeModule.controller("DisplayController", function($scope, $http, CrimeService){
   $scope.CrimeService = CrimeService;
   $scope.map = L.map('map').setView([35.595, -82.552], 12);
-  $scope.layer;
-  var geojsonMarkerOptions = {
+  var activeLayers = [];
+  var markerOpts = {
     radius: 5,
     fillColor: "#ff7800",
     color: "#000",
@@ -102,84 +100,48 @@ CrimeModule.controller("DisplayController", function($scope, $http, CrimeService
     { attribution: 'Map data © OpenStreetMap contributors' }
   ).addTo($scope.map);
 
-  $scope.getResults = function(){
-    //console.log("getting results");
-    postData = {
-      //severities: filterSelected(CrimeService.severities),
-      offenses: filterSelected(CrimeService.offenses),
-      //years: filterSelected(CrimeService.years)
-    }
-    //console.log("postData: "+ JSON.stringify(postData));
+  $scope.getOffenses = function(){
     $http({
       url: '/avlcrime/crimes',
       method: "POST",
-      data: postData,
       headers: {'Content-Type': 'application/json'}
     }).success(function (data, status, headers, config) {
-      //console.log("success");
-      //console.log(data);
-      if(typeof $scope.layer !== 'undefined'){
-        $scope.map.removeLayer($scope.layer);
-      }
-      $scope.layer = L.geoJson(data,{
-        onEachFeature: onEachFeature,
-        pointToLayer: function(feature, latlng){
-          //console.log("feature: "+ feature.properties.offense);
-          geojsonMarkerOptions.fillColor = FILL_COLORS[feature.properties.offense];
-          return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
-      }).addTo($scope.map);
+      $.each(data, function(offenseType, offenseGroup){
+        var tmpGrp = [];
+        markerOpts.fillColor = FILL_COLORS[offenseType];
+        $.each(offenseGroup, function(key, offense){
+          tmpGrp.push(
+            L.circleMarker(
+              [offense.lat, offense.lon], markerOpts
+            ).bindPopup(offense.date +"<br>"+ offense.add +"<br>"+ offense.off)
+          )
+        })
+        CrimeService.layers[offenseType] = L.layerGroup(tmpGrp);;
+      })
     }).error(function (data, status, headers, config) {
-      console.log("error")
+      //console.log("error")
     });
   }
 
-  //$scope.getResults();
+  $scope.getOffenses();
 
-  $scope.$watch('CrimeService.severities', function(newVal, oldVal, scope){
-    if(newVal !== oldVal){
-      //$scope.severities = filterSelected(newVal);
-      //console.log("display severities change: "+ JSON.stringify($scope.severities));
-      $scope.getResults();
-    }
-  }, true)
+  $scope.updateMap = function(){
+    $.each(CrimeService.offenses, function(key, val){
+      var layerIdx = $.inArray(val.name, activeLayers);
+      if(val.selected && (layerIdx < 0)){
+        activeLayers.push(val.name);
+        $scope.map.addLayer(CrimeService.layers[val.name]);
+      }else if(!val.selected && (layerIdx > -1)){
+        activeLayers.splice(layerIdx, 1);
+        $scope.map.removeLayer(CrimeService.layers[val.name]);
+      }
+    })
+  }
+
   $scope.$watch('CrimeService.offenses', function(newVal, oldVal, scope){
     if(newVal !== oldVal){
-      //$scope.offenses = filterSelected(newVal);
-      //console.log("display offenses change: "+ JSON.stringify($scope.offenses));
-      $scope.getResults();
-    }
-  }, true)
-  $scope.$watch('CrimeService.years', function(newVal, oldVal, scope){
-    if(newVal !== oldVal){
-      //$scope.years = filterSelected(newVal);
-      //console.log("display years change: "+ JSON.stringify($scope.years));
-      $scope.getResults();
+      $scope.updateMap();
     }
   }, true)
 })
 
-function onEachFeature(feature, layer) {
-  var popupContent = ""+ feature.properties.thedate.substring(0,10) +"<br>"+
-    feature.properties.offense +"<br>"+
-    //feature.properties.severity +": "+ feature.properties.offense +"<br>"+
-    feature.properties.address
-    
-  layer.bindPopup(popupContent);
-}
-
-function filterSelected(list){
-  //console.log("list: "+ list);
-  selected = [];
-  if(typeof list == 'undefined'){
-    return selected;
-  }
-  for(var i=0; i<list.length; i++){
-    if(list[i].selected == true){
-      selected.push(list[i].name);
-    }
-  }
-  return selected;
-}
-
-//angular.bootstrap(DMCalApp, ["DMCalApp"]);
